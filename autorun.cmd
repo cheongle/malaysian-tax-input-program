b824aca565299a917cb8de71315d2e01549e2aa4
Nicholas Cheong <jianlecheong@gmail.com>
2025-05-05 23:24:40 +0800

    Ignore unrelated files

--- autorun.cmd
+++
@@ -1,2 +0,0 @@
-cd "C:\Users\CHEONG YIEW KIANG\OneDrive\Documents\Bap Python"
-python test.py
--- test2.py
+++
@@ -1,8 +0,0 @@
-sequence = []
-a,b = 0,1
-for i in range(1, 100, 1):
-    sequence.append(a)
-    a,b = b, b + a
-i += 1
-
-print(sequence)
